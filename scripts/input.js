4294bf671eb1319b51ab9411141650da7c790a3f
mintra <mintrat@gmail.com>
2013-11-03 17:39:27 +0000

    adding interaction

---
+++ scripts/input.js
@@ -0,0 +1,22 @@
+swapGame.input = (function () {
+	var dom = swapGame.dom,
+		$ = dom.$,
+		settings = swapGame.settings,
+		inputHandlers;
+		
+	function initialise () {
+		inputHandlers = {};
+	}
+	
+	function bind (action, handler) {
+		// bind a handler function to a game action
+	}
+	
+	function trigger (action) {
+		// trigger a game action
+	}
+	
+	return {
+		initialise: initialise
+	}
+})();
